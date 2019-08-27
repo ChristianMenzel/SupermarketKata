e471ef99c2acf2202e79cd06772f4dbb4f9227b3
Christian Menzel <44576551+ChristianMenzel@users.noreply.github.com>
2019-08-27 22:52:44 +0200

    add description

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ console.log(queueTime([10,2,3,3], 2))
 console.log(queueTime([2,3,10], 2))
 
 function queueTime(queue, tills){
-	let singleQueues = new Array()
-	for (i = 0; i < tills; i++ ){ singleQueues[i] = 0}
+	// queues for the tills
+    let singleQueues = new Array()
+	
+    // init to them so can use _.min
+    for (i = 0; i < tills; i++ ){ singleQueues[i] = 0}
+    
+    // as long as there is something in queue
 	while (queue.length > 0){
 		// which singleQueue is shortest?
 		var maxKey = _.min(Object.keys(singleQueues),function(o){return singleQueues[o]})
+        
+        // add time from queue to shortest singleQueue
 		singleQueues[maxKey] += queue.shift()
 	}
+    
 	return _.max(singleQueues)
 }
